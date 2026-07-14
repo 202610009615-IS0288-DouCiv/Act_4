a5d77bbff25e7fe9ac43b59d526288ca5ec92a4b
DouCiv <cristian131412@gmail.com>
2026-07-13 21:47:17 -0500

    Se completa requerimiento de Nodo, se hace definicion de atributos, se da inicilizacion con el constrcutor y se crean los metodos

--- Nodo.java
+++ Nodo.java
@@ -1,3 +1,33 @@
 public class Nodo {
- 
+
+ private final String simbolo;
+ private final int linea;
+ private final int columna;
+ private Nodo siguiente;
+
+ public Nodo(String simbolo, int linea, int columna) {
+  this.simbolo = simbolo;
+  this.linea = linea;
+  this.columna = columna;
+
+ }
+
+ public String getSimbolo() {
+  return this.simbolo;
+ }
+
+ public int getLinea() {
+  return this.linea;
+ }
+
+ public int getColumna() {
+  return this.columna;
+
+ }
+
+ public void setSiguiente(Nodo siguiente) {
+
+  this.siguiente = siguiente;
+ }
+
 }
